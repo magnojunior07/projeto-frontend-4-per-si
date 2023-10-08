9f7337577c2eab7c52db34d6ab3aacc490464a0f
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2023-10-08 00:21:28 -0400

    style: finish style for Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './Pages/home/home.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProductFormComponent, ProductsListComponent],
+  declarations: [AppComponent, ProductFormComponent, ProductsListComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
